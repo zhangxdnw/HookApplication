158bd7380a86ae4c688a6ec37390c958fbbe6fe5
ZXD <zxd13510164430@gmail.com>
2021-08-01 20:47:18 +0800

    '使用宏晶库函数重新修改项目源码'

--- C51/main.c
+++ C51/main.c
@@ -1,367 +1,194 @@
-#include "stc8g.h"
-#include "intrins.h"
-#include "stdio.h"
-
-#define COMMAND_LENGTH 6
-#define MESSAGE_LENGTH 3
-
-//单片机定义
-#define ET4 0x40
-#define T4IF 0x04
-
-//消息定义
-typedef struct MESSAGE
-{
-	unsigned char command_data[MESSAGE_LENGTH];
-	int done;
-  struct message * next;
-}message;
-
-
-//全局变量定义
-unsigned char recv_char = 0;
-char recv_count = 0;
-int time4_count = 0;
-unsigned char origin_command[COMMAND_LENGTH];
-unsigned char decode_message[MESSAGE_LENGTH];
-unsigned char encode_message[MESSAGE_LENGTH];
-unsigned char return_data[COMMAND_LENGTH];
-message * iterator;
-
-//全局函数声明
-void decode(unsigned char *src, unsigned char *dst);
-void encode(unsigned char *src, unsigned char *dst);
-void add_new_message(unsigned char *msg_data);
-void Delay1ms();
-void send_key_code(int key_code);
-
-//系统定义函数
-
 /***
-	* 串口1初始化函数
+	*
+	* 使用宏晶示例代码修改的程序
 	*/
-void uart_1_init(void)	//115200bps@35MHz	
-{
-	SCON = 0x50;		//8位数据,可变波特率
-	AUXR |= 0x01;		//串口1选择定时器2为波特率发生器
-	AUXR |= 0x04;		//定时器时钟1T模式
-	T2L = 0xB4;		//设置定时初始值
-	T2H = 0xFF;		//设置定时初始值
-	AUXR |= 0x10;		//定时器2开始计时
-}
-
-/***
-	* 串口3初始化函数
-	*/
-void uart_3_init(void)
-{
-	S3CON = 0x10;		//8位数据,可变波特率
-	S3CON &= 0xBF;		//串口3选择定时器2为波特率发生器
-	AUXR |= 0x04;		//定时器时钟1T模式
-	T2L = 0xB4;		//设置定时初始值
-	T2H = 0xFF;		//设置定时初始值
-	AUXR |= 0x10;		//定时器2开始计时
-}
-
-/***
-	* 串口4初始化函数
-	*/
-void uart_4_init(void)		//115200bps@35.000MHz
-{
-	S4CON = 0x10;		//8位数据,可变波特率
-	S4CON &= 0xBF;		//串口4选择定时器2为波特率发生器
-	AUXR |= 0x04;		//定时器时钟1T模式
-	T2L = 0xB4;		//设置定时初始值
-	T2H = 0xFF;		//设置定时初始值
-	AUXR |= 0x10;		//定时器2开始计时
-}
-
-void timer_4_init()	//100微秒@35MHz
-{
-	T4L = 0x48;		//设置定时初始值
-	T4H = 0x77;		//设置定时初始值
-	IE2 = ET4;
-}
-
-/***
-	* 串口1数据发送
-	*/
-void send_data_uart_1(unsigned char *source, int length)
-{
-	int i;
-	for(i =0;i<length;i++)
-	{
-		SBUF = *(source+i);
-		while(!TI);
-		TI = 0;
-	}
-}
-
-/***
-	* 串口3数据发送
-	*/
-void send_data_uart_3(unsigned char *source, int length)
-{
-	int i;
-	for(i =0;i<length;i++)
-	{
-		S3BUF = *(source+i);
-		Delay1ms();
-	}
-}
+#include "stdlib.h"
+#include "config.h"
+#include "gpio.h"
+#include "UART.h"
+#include "delay.h"
+#include "Exti.h"
+#include "timer.h"
+#include "adc.h"
 
 
-/***
-	* 串口4数据发送
-	*/
-void send_data_uart_4(unsigned char *source, int length)
-{
-	int i;
-	for(i =0;i<length;i++)
-	{
-		S4BUF = *(source+i);
-		Delay1ms();
-	}
-}
+/*************	本地常量声明	**************/
+#define COMMAND_LENGTH 8
+#define MESSAGE_LENGTH 4
 
-/***
-	* INT0中断，用作放板感应
-	*/
-void int0_isr() interrupt 0
-{
-	if(T4T3M == 0x80)	//timer4已经开启
-	{
-		time4_count = 0;
-	}
-	else
-	{
-		time4_count = 0;
-		T4T3M = 0x80;
-	}
-}
+/*************	本地变量声明	**************/
+int timer0_count = 0;
+u8 encode_message[MESSAGE_LENGTH];
+u8 decode_message[MESSAGE_LENGTH];
+u8 return_data[COMMAND_LENGTH];
+u16	adc_value;
 
-/***
-	* 定时器4中断，放板后定时触发
-	*/
-void time4_isr() interrupt 20
-{
-	if(++time4_count > 400)	//4秒后按下按键
-	{
-		time4_count = 0;
-		//send_key_code(0x29);	//发送Esc按键
-		SBUF = 0xAB;
-		while(!TI);
-		TI=0;
-		T4T3M = 0x00;
-		AUXINTIF &= ~T4IF;
-	}
-}
+/*************	本地函数声明	**************/
 
-/***
-	* 串口1中断，串口1消息接收
-	*/
-void uart1_isr() interrupt 4
-{
-	if(RI == 1)	//recv
-	{
-		recv_char = SBUF;
-		RI = 0;
-		if(recv_count<COMMAND_LENGTH)
-		{
-			origin_command[recv_count++] = recv_char;
-			if(recv_count >= COMMAND_LENGTH)
-			{
-				decode(origin_command, decode_message);
-				//send_data_uart_1(decode_message, MESSAGE_LENGTH);
-				add_new_message(decode_message);
-				recv_count = 0;
-			}
-		}
-	}
-}
 
-/***
-	* 串口4中断，串口4消息接收
-	*	 接收语音模块发送过来的数据，并转发到串口1
-	*/
-void uart4_isr() interrupt 18
+
+/*************  外部函数和变量声明 *****************/
+
+
+/******************* IO配置函数 *******************/
+void	GPIO_config(void)
 {
+	GPIO_InitTypeDef	GPIO_InitStructure;		//结构定义
+
+	//COM1 P3.0 P3.1
+	GPIO_InitStructure.Pin  = GPIO_Pin_0 | GPIO_Pin_1;		//指定要初始化的IO, GPIO_Pin_0 ~ GPIO_Pin_7
+	GPIO_InitStructure.Mode = GPIO_PullUp;	//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
+	GPIO_Inilize(GPIO_P3,&GPIO_InitStructure);	//初始化
 	
+	//INT0 P3.2
+	GPIO_InitStructure.Pin  = GPIO_Pin_2;			//指定要初始化的IO, GPIO_Pin_0 ~ GPIO_Pin_7, 或操作
+	GPIO_InitStructure.Mode = GPIO_PullUp;			//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
+	GPIO_Inilize(GPIO_P3,&GPIO_InitStructure);	//初始化
+	
+	//COM3 P5.0 P5.1
+	GPIO_InitStructure.Pin  = GPIO_Pin_0 | GPIO_Pin_1;		//指定要初始化的IO, GPIO_Pin_0 ~ GPIO_Pin_7
+	GPIO_InitStructure.Mode = GPIO_PullUp;	//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
+	GPIO_Inilize(GPIO_P5,&GPIO_InitStructure);	//初始化
+	
+	//COM4 P5.2 P5.3
+	GPIO_InitStructure.Pin  = GPIO_Pin_2 | GPIO_Pin_3;		//指定要初始化的IO, GPIO_Pin_0 ~ GPIO_Pin_7
+	GPIO_InitStructure.Mode = GPIO_PullUp;	//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
+	GPIO_Inilize(GPIO_P5,&GPIO_InitStructure);	//初始化
+	
+	//ADC 0-14 P1.0~P1.7 P0.0~P0.7
+	GPIO_InitStructure.Pin  = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;		//指定要初始化的IO, GPIO_Pin_0 ~ GPIO_Pin_7
+	GPIO_InitStructure.Mode = GPIO_PullUp;	//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
+	GPIO_Inilize(GPIO_P1,&GPIO_InitStructure);	//初始化
+	//GPIO_Inilize(GPIO_P0,&GPIO_InitStructure);	//初始化
 }
 
-void IapIdle() 
-{ 
-	IAP_CONTR = 0; //关闭 IAP 功能
-	IAP_CMD = 0; //清除命令寄存器
-	IAP_TRIG = 0; //清除触发寄存器
-	IAP_ADDRH = 0x80; //将地址设置到非 IAP 区域
-	IAP_ADDRL = 0; 
-} 
-
-char IapRead(int addr) 
-{ 
-	char dat; 
-
-	IAP_CONTR = 0x80; //使能 IAP
-	IAP_TPS = 12; //设置擦除等待参数 12MHz
-	IAP_CMD = 1; //设置 IAP 读命令
-	IAP_ADDRL = addr; //设置 IAP 低地址
-	IAP_ADDRH = addr >> 8; //设置 IAP 高地址
-	IAP_TRIG = 0x5a; //写触发命令(0x5a)
-	IAP_TRIG = 0xa5; //写触发命令(0xa5)
-	_nop_(); 
-	dat = IAP_DATA; //读 IAP 数据
-	IapIdle(); //关闭 IAP 功能
-	return dat; 
-} 
-
-/***
-	* ADC读取函数
-	*	STC8G2K64S4 ADC通道
-	*	ADC0	P1.0
-	*	ADC1	P1.1
-	*	ADC2	P1.2
-	*	ADC3	P1.3
-	*	ADC4	P1.4
-	*	ADC5	P1.5
-	*	ADC6	P1.6
-	*	ADC7	P1.7
-	*	ADC8	P0.0
-	*	ADC9	P0.1
-	*	ADC10	P0.2
-	*	ADC11	P0.3
-	*	ADC12	P0.4
-	*	ADC13	P0.5
-	*	ADC14	P0.6
-	*	ADC15	P0.7
-	*/
-void get_ad_value(int channel)
+/***************  串口初始化函数 *****************/
+void	UART_config(void)
 {
-	unsigned int ret;
+	COMx_InitDefine		COMx_InitStructure;					//结构定义
 	
-	ADCCFG = 0x2f; //设置 ADC 时钟为系统时钟/2/16/16
-	switch(channel)
+	//COM1
+	COMx_InitStructure.UART_Mode      = UART_8bit_BRTx;		//模式,       UART_ShiftRight,UART_8bit_BRTx,UART_9bit,UART_9bit_BRTx
+	COMx_InitStructure.UART_BRT_Use   = BRT_Timer1;			//使用波特率,   BRT_Timer1, BRT_Timer2 (注意: 串口2固定使用BRT_Timer2)
+	COMx_InitStructure.UART_BaudRate  = 115200ul;			//波特率, 一般 110 ~ 115200
+	COMx_InitStructure.UART_RxEnable  = ENABLE;				//接收允许,   ENABLE或DISABLE
+	COMx_InitStructure.BaudRateDouble = DISABLE;			//波特率加倍, ENABLE或DISABLE
+	COMx_InitStructure.UART_Interrupt = ENABLE;				//中断允许,   ENABLE或DISABLE
+	COMx_InitStructure.UART_Polity    = Polity_0;			//指定中断优先级(低到高) Polity_0,Polity_1,Polity_2,Polity_3
+	COMx_InitStructure.UART_P_SW      = UART1_SW_P30_P31;	//切换端口,   UART1_SW_P30_P31,UART1_SW_P36_P37,UART1_SW_P16_P17,UART1_SW_P43_P44
+	UART_Configuration(UART1, &COMx_InitStructure);		//初始化串口1
+	
+	//COM3
+	COMx_InitStructure.UART_Mode      = UART_8bit_BRTx;		//模式,   UART_ShiftRight,UART_8bit_BRTx,UART_9bit,UART_9bit_BRTx
+	COMx_InitStructure.UART_BRT_Use   = BRT_Timer3;			//使用波特率,   BRT_Timer2, BRT_Timer3 (注意: 串口2固定使用BRT_Timer2)
+	COMx_InitStructure.UART_BaudRate  = 115200ul;			//波特率,     110 ~ 115200
+	COMx_InitStructure.UART_RxEnable  = ENABLE;				//接收允许,   ENABLE或DISABLE
+	COMx_InitStructure.UART_Interrupt = ENABLE;				//中断允许,   ENABLE或DISABLE
+	COMx_InitStructure.UART_Polity    = Polity_0;			//指定中断优先级(低到高) Polity_0,Polity_1,Polity_2,Polity_3
+	COMx_InitStructure.UART_P_SW      = UART3_SW_P50_P51;	//切换端口,   UART3_SW_P00_P01,UART3_SW_P50_P51
+	UART_Configuration(UART3, &COMx_InitStructure);		//初始化串口3
+	
+	//COM4
+	COMx_InitStructure.UART_Mode      = UART_8bit_BRTx;		//模式,   UART_ShiftRight,UART_8bit_BRTx,UART_9bit,UART_9bit_BRTx
+	COMx_InitStructure.UART_BRT_Use   = BRT_Timer4;			//使用波特率,   BRT_Timer2, BRT_Timer4 (注意: 串口2固定使用BRT_Timer2)
+	COMx_InitStructure.UART_BaudRate  = 115200ul;			//波特率,     110 ~ 115200
+	COMx_InitStructure.UART_RxEnable  = ENABLE;				//接收允许,   ENABLE或DISABLE
+	COMx_InitStructure.UART_Interrupt = ENABLE;				//中断允许,   ENABLE或DISABLE
+	COMx_InitStructure.UART_Polity    = Polity_0;			//指定中断优先级(低到高) Polity_0,Polity_1,Polity_2,Polity_3
+	COMx_InitStructure.UART_P_SW      = UART4_SW_P52_P53;	//切换端口,   UART4_SW_P02_P03,UART4_SW_P52_P53
+	UART_Configuration(UART4, &COMx_InitStructure);
+}
+
+/******************** INT配置 ********************/
+void	Exti_config(void)
+{
+	EXTI_InitTypeDef	Exti_InitStructure;							//结构定义
+
+	Exti_InitStructure.EXTI_Interrupt = ENABLE;				//中断使能,   ENABLE或DISABLE
+	Exti_InitStructure.EXTI_Mode      = EXT_MODE_Fall;//中断模式,   EXT_MODE_RiseFall,EXT_MODE_Fall
+	Exti_InitStructure.EXTI_Polity    = Polity_0;			//指定中断优先级(低到高) Polity_0,Polity_1,Polity_2,Polity_3
+	Ext_Inilize(EXT_INT0,&Exti_InitStructure);				//初始化
+}
+
+/************************ 定时器配置 ****************************/
+void	Timer_config(void)
+{
+	TIM_InitTypeDef		TIM_InitStructure;						//结构定义
+	TIM_InitStructure.TIM_Mode      = TIM_16BitAutoReload;	//指定工作模式,   TIM_16BitAutoReload,TIM_16Bit,TIM_8BitAutoReload,TIM_16BitAutoReloadNoMask
+	TIM_InitStructure.TIM_Polity    = Polity_0;			//指定中断优先级(低到高) Polity_0,Polity_1,Polity_2,Polity_3
+	TIM_InitStructure.TIM_Interrupt = ENABLE;					//中断是否允许,   ENABLE或DISABLE
+	TIM_InitStructure.TIM_ClkSource = TIM_CLOCK_1T;		//指定时钟源,     TIM_CLOCK_1T,TIM_CLOCK_12T,TIM_CLOCK_Ext
+	TIM_InitStructure.TIM_ClkOut    = DISABLE;				//是否输出高速脉冲, ENABLE或DISABLE
+	TIM_InitStructure.TIM_Value     = 65536UL - (MAIN_Fosc / (50*12));		//初值,
+	TIM_InitStructure.TIM_Run       = DISABLE;					//是否初始化后启动定时器, ENABLE或DISABLE
+	Timer_Inilize(Timer0,&TIM_InitStructure);					//初始化Timer0
+}
+
+/******************* AD配置函数 *******************/
+void	ADC_config(void)
+{
+	ADC_InitTypeDef		ADC_InitStructure;		//结构定义
+	ADC_InitStructure.ADC_SMPduty   = 31;		//ADC 模拟信号采样时间控制, 0~31（注意： SMPDUTY 一定不能设置小于 10）
+	ADC_InitStructure.ADC_CsSetup   = 0;		//ADC 通道选择时间控制 0(默认),1
+	ADC_InitStructure.ADC_CsHold    = 1;		//ADC 通道选择保持时间控制 0,1(默认),2,3
+	ADC_InitStructure.ADC_Speed     = ADC_SPEED_2X1T;		//设置 ADC 工作时钟频率	ADC_SPEED_2X1T~ADC_SPEED_2X16T
+	ADC_InitStructure.ADC_Power     = ENABLE;				//ADC功率允许/关闭	ENABLE,DISABLE
+	ADC_InitStructure.ADC_AdjResult = ADC_RIGHT_JUSTIFIED;	//ADC结果调整,	ADC_LEFT_JUSTIFIED,ADC_RIGHT_JUSTIFIED
+	ADC_InitStructure.ADC_Polity    = Polity_0;			//指定中断优先级(低到高) Polity_0,Polity_1,Polity_2,Polity_3
+	ADC_InitStructure.ADC_Interrupt = DISABLE;			//中断允许	ENABLE,DISABLE
+	ADC_Inilize(&ADC_InitStructure);					//初始化
+	ADC_PowerControl(ENABLE);							//单独的ADC电源操作函数, ENABLE或DISABLE
+}
+
+void send_data_uart_1(unsigned char *buf, u8 length)
+{
+	u8 i = 0;
+	for(i=0; i<length; i++)
+		TX1_write2buff(*(buf + i));
+}
+
+void send_data_uart_3(unsigned char *buf, u8 length)
+{
+	u8 i = 0;
+	for(i=0; i<length; i++)
+		TX3_write2buff(*(buf + i));
+}
+
+void send_data_uart_4(unsigned char *buf, u8 length)
+{
+	u8 i = 0;
+	for(i=0; i<length; i++)
+		TX4_write2buff(*(buf + i));
+}
+
+/********************* Timer0中断函数************************/
+void timer0_int (void) interrupt TIMER0_VECTOR
+{
+	if(timer0_count++ >= 65 * 40)		//经验值65大概100ms
 	{
-		case 0:
-			P1M0 = 0x00; //设置 P1.0 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x80; //使能 ADC 模块
-			break;
-		case 1:
-			P1M0 = 0x00; //设置 P1.1 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x81; //使能 ADC 模块
-			break;
-		case 2:
-			P1M0 = 0x00; //设置 P1.2 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x82; //使能 ADC 模块
-			break;
-		case 3:
-			P1M0 = 0x00; //设置 P1.3 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x83; //使能 ADC 模块
-			break;
-		case 4:
-			P1M0 = 0x00; //设置 P1.4 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x84; //使能 ADC 模块
-			break;
-		case 5:
-			P1M0 = 0x00; //设置 P1.5 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x85; //使能 ADC 模块
-			break;
-		case 6:
-			P1M0 = 0x00; //设置 P1.6 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x86; //使能 ADC 模块
-			break;
-		case 7:
-			P1M0 = 0x00; //设置 P1.7 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x87; //使能 ADC 模块
-			break;
-		case 8:
-			P1M0 = 0x00; //设置 P0.0 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x88; //使能 ADC 模块
-			break;
-		case 9:
-			P1M0 = 0x00; //设置 P0.1 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x89; //使能 ADC 模块
-			break;
-		case 10:
-			P1M0 = 0x00; //设置 P0.2 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x8A; //使能 ADC 模块
-			break;
-		case 11:
-			P1M0 = 0x00; //设置 P0.3 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x8B; //使能 ADC 模块
-			break;
-		case 12:
-			P1M0 = 0x00; //设置 P0.4 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x8C; //使能 ADC 模块
-			break;
-		case 13:
-			P1M0 = 0x00; //设置 P0.5 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x8D; //使能 ADC 模块
-			break;
-		case 14:
-			P1M0 = 0x00; //设置 P0.6 为 ADC 口
-			P1M1 = 0x01; 
-			ADC_CONTR = 0x8E; //使能 ADC 模块
-			break;
-		default:
-			return;
+		timer0_count = 0;
+		Timer0_Stop();
 	}
-	ADC_CONTR |= 0x40; //启动 AD 转换
-	_nop_(); 
-	_nop_(); 
-	while (!(ADC_CONTR & 0x20)); //查询 ADC 完成标志
-	ADC_CONTR &= ~0x20; //清完成标志
 }
 
-/***
-	* 延时函数
-	*/
-void Delay1ms()		//@35MHz
+void encode(u8 *src, u8 *dst, u8 seed)
 {
-	unsigned char i, j;
-
-	_nop_();
-	_nop_();
-	i = 46;
-	j = 113;
-	do
+	u8 i;
+	srand(seed);
+	for(i = 0; i < MESSAGE_LENGTH; i++)
 	{
-		while (--j);
-	} while (--i);
+		*(dst + i) = rand();
+		*(dst + i + MESSAGE_LENGTH) = *(dst + i) ^ *(src + i);
+	}
 }
 
-//自定义函数
-
-/***
-	* 初始化函数
-	*/
-void init()
+void decode(u8 *src, u8 *dst)
 {
-	iterator = 0;
-	EA = 1;	//所有中断开关
-	
-	ES = 1;	//串口1中断开关
-	
-	IT0 = 1;	//INT0 下降沿中断
-	EX0 = 1;	//INT0 外部中断开关
-	
-	uart_1_init();	//串口1初始化
-	uart_3_init();	//串口1初始化
-	timer_4_init();	//定时器4初始化
+	u8 i;
+	for(i = 0; i < MESSAGE_LENGTH; i++)
+	{
+		*(dst + i) = *(src + i) ^ *(src + i + MESSAGE_LENGTH);
+	}
 }
 
 /***
@@ -373,143 +200,124 @@ void send_key_code(int key_code)
 	unsigned char key_up[8] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
 	key_down[2] = key_code;
 	send_data_uart_3(key_down, 8);
-	Delay1ms();
+	delay_ms(1);
 	send_data_uart_3(key_up, 8);
 }
 
-/***
-	* 命令执行函数
-	*/
-void execute(unsigned char *source)
+/********************* 串口1命令执行函数************************/
+void execute(u8 *source)
 {
-	switch(*source)
+	decode(source, decode_message);
+	switch(decode_message[0])
 	{
 		case 0x01:	//握手，原数据返回
-			encode_message[0] = *source;
-			encode_message[1] = *(source+1);
-			encode_message[2] = *(source+2);
-			encode(encode_message, return_data);
+			encode_message[0] = decode_message[0];
+			encode_message[1] = decode_message[1];
+			encode_message[2] = decode_message[2];
+			encode_message[3] = decode_message[3];
+			encode(encode_message, return_data, *source);
 			send_data_uart_1(return_data, COMMAND_LENGTH);
 			break;
 		case 0x02:	//读取单片机信息
-			encode_message[0] = *source;
-			encode_message[1] = IapRead(0x0001);
-			encode_message[2] = IapRead(0x0002);
-			encode(encode_message, return_data);
+			encode_message[0] = decode_message[0];
+			encode_message[1] = 0x00;
+			encode_message[2] = 0x00;
+			encode_message[3] = 0x00;
+			encode(encode_message, return_data, *source);
 			send_data_uart_1(return_data, COMMAND_LENGTH);
 			break;
 		case 0x03:	//读取ADC指定通道电压
-			get_ad_value(*(source+1));
-			encode_message[0] = *source;
-			encode_message[1] = ADC_RES;
-			encode_message[2] = ADC_RESL;
-			encode(encode_message, return_data);
+			ADC_PowerControl(ENABLE);							//单独的ADC电源操作函数, ENABLE或DISABLE
+			adc_value = Get_ADCResult(decode_message[1]);
+			encode_message[0] = decode_message[0];
+			encode_message[1] = decode_message[1];
+			encode_message[2] = adc_value>>8;
+			encode_message[3] = (u8)adc_value;
+			//send_data_uart_1(encode_message, MESSAGE_LENGTH);
+			encode(encode_message, return_data, *source);
 			send_data_uart_1(return_data, COMMAND_LENGTH);
+			ADC_PowerControl(DISABLE);							//单独的ADC电源操作函数, ENABLE或DISABLE
 			break;
 		case 0x04:	//识别语音
+			//TODO 开始识别语音
 			break;
 		case 0x05:	//发送指定按键
-			send_key_code(*(source+1));
-			encode_message[0] = *source;
-			encode_message[1] = 0x00;
+			send_key_code(decode_message[1]);
+			encode_message[0] = decode_message[0];
+			encode_message[1] = decode_message[1];
 			encode_message[2] = 0x00;
-			encode(encode_message, return_data);
+			encode_message[3] = 0x00;
+			encode(encode_message, return_data, *source);
 			send_data_uart_1(return_data, COMMAND_LENGTH);
 			break;
 	}
 }
 
-/***
-	* 延时函数n毫秒
-	*/
-void delay_n_ms(int n)
+/********************* 串口4命令执行函数************************/
+void send_speech_recognition(u8 result)
 {
-	int i = 0;
-	for(i;i<n;i++)
-	{
-		Delay1ms();
-	}
+	encode_message[0] = 0x04;
+	encode_message[1] = result;
+	encode_message[2] = 0x00;
+	encode_message[3] = 0x00;
+	encode(encode_message, return_data, result);
+	send_data_uart_1(return_data, COMMAND_LENGTH);
 }
 
-/***
-	* 加密函数 随机产生3字节，与后面消息内容异或加密
-	*/
-void encode(unsigned char *src, unsigned char *dst)
+/**********************************************/
+void main(void)
 {
-	int index = 0;
-	for(index; index < MESSAGE_LENGTH; index++)
-	{
-		unsigned char random_data = rand()>>8;
-		*(dst+index) = random_data;
-		*(dst+index + MESSAGE_LENGTH) = * (src + index) ^ *(dst+index);
-	}
-}
+	u8	i;
 
-/***
-	* 解密函数，异或解密
-	*/
-void decode(unsigned char *src, unsigned char *dst)
-{
-	int index = 0;
-	for(index; index < MESSAGE_LENGTH; index++)
-	{
-		*(dst+index) = *(src+index) ^ * (src+index+MESSAGE_LENGTH);
-	}
-}
+	GPIO_config();
+	UART_config();
+	Exti_config();
+	Timer_config();
+	ADC_config();
+	EA = 1;
 
-/***
-	* 消息添加到队列函数
-	*/
-void add_message_to_list(message msg)
-{
-	if(iterator == 0)
+	while (1)
 	{
-		iterator = &msg;
-	}
-	else
-	{
-		message *tmp = iterator;
-		while(tmp->next != 0)
+		delay_ms(1);
+		if(COM1.RX_TimeOut > 0)		//超时计数
 		{
-			tmp = tmp->next;
-		}
-		tmp->next = &msg;
-	}
-}
-
-/***
-	* 生成新消息函数
-	*/
-void add_new_message(unsigned char * msg_data)
-{
-	message msg;
-	int i;
-	for(i = 0; i < MESSAGE_LENGTH; i++)
-	{
-		msg.command_data[i] = *(msg_data + i);
-	}
-	msg.next = 0;
-	msg.done = 0;
-	add_message_to_list(msg);
-}
-
-/***
-	* 主函数
-	*/
-void main()
-{
-	init();
-
-	while(1)
-	{
-		if(iterator != 0)
-		{
-			if(iterator->done == 0)
+			if(--COM1.RX_TimeOut == 0)
 			{
-				execute(iterator->command_data);
-				iterator->done = 1;
-				iterator = iterator->next;
+				if(COM1.RX_Cnt > 0)
+				{
+					execute(RX1_Buffer);
+				}
+				COM1.RX_Cnt = 0;
 			}
+		}
+		if(COM3.RX_TimeOut > 0)		//超时计数
+		{
+			if(--COM3.RX_TimeOut == 0)
+			{
+				if(COM3.RX_Cnt > 0)
+				{
+					for(i=0; i<COM3.RX_Cnt; i++)	TX3_write2buff(RX3_Buffer[i]);	//收到的数据原样返回
+				}
+				COM3.RX_Cnt = 0;
+			}
+		}
+		if(COM4.RX_TimeOut > 0)		//超时计数
+		{
+			if(--COM4.RX_TimeOut == 0)
+			{
+				if(COM4.RX_Cnt > 0)
+				{
+					//TODO 串口4数据返回给上位机
+					send_speech_recognition(0x00);
+				}
+				COM4.RX_Cnt = 0;
+			}
+		}
+		if(!INT0)	//中断INT0产生
+		{
+			TX1_write2buff(0xAA);
+			timer0_count=0;
+			Timer0_Run();
 		}
 	}
 }
